d5033efc4a046d3918df9cc8643522a3147455f1
rtw <ryan.wheaton@gmail.com>
2016-11-01 21:31:29 -0600

    alpine no worky.  too much, not enough time.

--- alpine-nginx-php-mysql-app/dev-run.sh
+++
@@ -1,9 +0,0 @@
-docker build -t alpine-nginx-php-mysql-app .
-
-docker run -d \
-  --name nginx-php-mysql \
-  -p 80:80 \
-  -v $PWD/data:/var/lib/mysql \
-  -v $PWD/logs:/data/logs \
-  -v $PWD:/data/htdocs \
-  alpine-nginx-php-mysql-app
--- alpine-nginx-php-mysql-app/src/db-connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  define('DB_NAME', getenv('MYSQL_DB_NAME'));
-  define('DB_USER', getenv('MYSQL_USERNAME'));
-  define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
-  define('DB_HOST', getenv('MYSQL_HOSTNAME'));
-  define('DB_TABLE', 'urler');
-?>
